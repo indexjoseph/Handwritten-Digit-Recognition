0d4f128216c03767e413064a18dbdbd6e2a677fc
Brady Smith <bksmith8@catamount.wcu.edu>
2023-02-17 16:58:56 -0500

    Changed files to get GUI to work and changed the training data

--- bot.py
+++ bot.py
@@ -7,18 +7,24 @@ from keras.layers import Flatten
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras import backend as K
-
-# making a training and testing set.
+#  to split the data of training and testing sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+
+batch_size = 128
+num_classes = 10
+epochs = 20
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 input_shape = (28, 28, 1)
+# conversion of class vectors to matrices of  binary class
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
 
-# creating a model
-batch_size = 128
-num_classes = 10
-epochs = 10
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
 model.add(Conv2D(64, (3, 3), activation='relu'))
@@ -30,15 +36,6 @@ model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 model.compile(loss=keras.losses.categorical_crossentropy,optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
 
-#convertin of vectors to binary matrices
-y_train = keras.utils.to_categorical(y_train, 10)
-y_test = keras.utils.to_categorical(y_test, 10)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-
-#training the model
 hist = model.fit(x_train, y_train,batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(x_test, y_test))
 print("The model has successfully trained")
 model.save('mnist.h5')
